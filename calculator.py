beb29f3849483bb81214b243fe58472ba237947c
Abhishek <abhi905040@gamil.com>
2023-10-04 19:25:24 +0530

    define function

--- calculator.py
+++ calculator.py
@@ -1,4 +1,14 @@
-x= int(input("whats x?"))
-y= int(input("whats y?"))
+# integer addition
+# x= int(input("whats x?"))
+# y= int(input("whats y?"))
 
-print(x+y)
+# print(x+y)
+
+# float addition
+a= float(input("whats a?"))
+b= float(input("whats b?"))
+
+# use of round function 
+z = round(a+b)
+# print(z);
+print(f"{z:,}");
---
+++ def.py
@@ -0,0 +1,16 @@
+# def is use for defining the function
+def main():
+    hello();
+    name = input("What's your name")
+    hello(name)
+
+
+def hello(to="world"):
+    print("hello" , to)
+
+main()
+
+# here i call the main function first and then i call the hello function inside from the main function
+
+
+
